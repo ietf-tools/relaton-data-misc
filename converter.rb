882f45110761021a020d4e8b0cd4410e71e5fd6d
Andrei Kislichenko <android.2net@gmail.com>
2022-05-11 14:38:22 +0200

    document identifier normalization

--- converter.rb
+++ converter.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
-require 'relaton_ietf'
+require 'relaton_ieee'
 
 Dir.mkdir 'data' unless Dir.exist? 'data'
 
 Dir['./bibxml-data-misc/data/reference.*.xml'].each do |file|
   xml = File.read file, encoding: 'UTF-8'
-  bib = RelatonIetf::BibXMLParser.parse xml
+  doc = Nokogiri::XML xml
+  ref = doc.at '/reference'
+  type = ref[:anchor].split('.').first
+  bib = case type
+        when 'IEEE' then RelatonIeee::BibXMLParser.parse xml
+        else RelatonBib::BibXMLParser.parse xml
+        end
   filename = File.join 'data', "#{File.basename(file, '.xml')}.yaml"
   File.write filename, bib.to_hash.to_yaml, encoding: 'UTF-8'
 end
